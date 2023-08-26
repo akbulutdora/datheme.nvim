ef4f9bc6db8e014e79bdb648bd22b8a070460e62
Dora Akbulut <akbulutdora@gmail.com>
2023-08-26 12:45:00 +0100

    first colors

--- lua/nord/named_colors.lua
+++ lua/nord/named_colors.lua
@@ -1,22 +1,35 @@
+-- My colors
+-- bg - #e2e2e2
+-- yellow - #ffdd33 (control, storage.type)
+-- green - #79bf46 (string,)
+-- storage? - ff0000
+-- orange - #ff9b21 (comment)
+-- macro - #bfc5bf
+-- const - ##bfc5bf
+-- error - #F44747
+-- warn - #CD9731
+-- debug - #B267E6
+-- info - #6796E6
+--
 local nord = {
-	--16 colors
-	black = "#2E3440", -- nord0 in palette
-	dark_gray = "#3B4252", -- nord1 in palette
-	gray = "#434C5E", -- nord2 in palette
-	light_gray = "#4C566A", -- nord3 in palette
-	light_gray_bright = "#616E88", -- out of palette
-	darkest_white = "#D8DEE9", -- nord4 in palette
-	darker_white = "#E5E9F0", -- nord5 in palette
-	white = "#ECEFF4", -- nord6 in palette
-	teal = "#8FBCBB", -- nord7 in palette
-	off_blue = "#88C0D0", -- nord8 in palette
-	glacier = "#81A1C1", -- nord9 in palette
-	blue = "#5E81AC", -- nord10 in palette
-	red = "#BF616A", -- nord11 in palette
-	orange = "#D08770", -- nord12 in palette
-	yellow = "#EBCB8B", -- nord13 in palette
-	green = "#A3BE8C", -- nord14 in palette
-	purple = "#B48EAD", -- nord15 in palette
-	none = "NONE",
+    -- 16 colors
+    black = "#E2E2E2", -- x nord0 in palette
+    dark_gray = "#3B4252", -- nord1 in palette
+    gray = "#434C5E", -- nord2 in palette
+    light_gray = "#4C566A", -- nord3 in palette
+    light_gray_bright = "#616E88", -- out of palette
+    darkest_white = "#D8DEE9", -- nord4 in palette
+    darker_white = "#E5E9F0", -- nord5 in palette
+    white = "#ECEFF4", -- nord6 in palette
+    teal = "#8FBCBB", -- nord7 in palette
+    off_blue = "#88C0D0", -- nord8 in palette
+    glacier = "#81A1C1", -- nord9 in palette
+    blue = "#5E81AC", -- nord10 in palette
+    red = "#FF0000", -- x nord11 in palette
+    orange = "#FF9B21", -- x nord12 in palette
+    yellow = "#FFDD33", -- x nord13 in palette
+    green = "#79BF46", -- x nord14 in palette
+    purple = "#B48EAD", -- nord15 in palette
+    none = "NONE"
 }
 return nord
